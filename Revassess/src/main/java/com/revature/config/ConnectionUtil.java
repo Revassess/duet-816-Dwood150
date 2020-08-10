a48280dcb8d4f0b81adc1862205ed3f41be0a411
Dakota Wood <cody.wood1943@gmail.com>
2020-08-10 17:19:16 -0400

    tier3456

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -16,11 +16,11 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:postgresql://jul13-2007-java-august.cusb4vetdsec.us-east-1.rds.amazonaws.com:5432/test";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "dakota_wood";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "revature01";
 	// name of the created stored procedure in tier 3
 	public static final String TIER_3_PROCEDURE_NAME = "";
 	// name of the created sequence in tier 3
